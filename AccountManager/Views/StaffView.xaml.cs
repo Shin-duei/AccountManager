477e8b716de577e49a3602b694e657cb462d49d3
Xindzhou <xindzhou@pwrvalue.com>
2022-09-04 23:21:28 +0800

    Add Staff

---
+++ AccountManager/Views/StaffView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountManager.Views
+{
+    /// <summary>
+    /// StaffView.xaml 的互動邏輯
+    /// </summary>
+    public partial class StaffView : UserControl
+    {
+        public StaffView()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        private void ShowPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            passwordTxtBox.Text = passwordBox.Password;
+            passwordBox.Visibility = Visibility.Collapsed;
+            passwordTxtBox.Visibility = Visibility.Visible;
+        }
+
+        private void ShowPassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            passwordBox.Password = passwordTxtBox.Text;
+            passwordTxtBox.Visibility = Visibility.Collapsed;
+            passwordBox.Visibility = Visibility.Visible;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsPasswordShow)
+            {
+                passwordTxtBox.Text = passwordBox.Password;
+                passwordBox.Visibility = Visibility.Collapsed;
+                passwordTxtBox.Visibility = Visibility.Visible;
+
+                var button = (Button)sender;
+                button.Content = new MaterialDesignThemes.Wpf.PackIcon { Kind = MaterialDesignThemes.Wpf.PackIconKind.EyeOff };
+                IsPasswordShow = true;
+            }
+            else
+            {
+                passwordBox.Password = passwordTxtBox.Text;
+                passwordTxtBox.Visibility = Visibility.Collapsed;
+                passwordBox.Visibility = Visibility.Visible;
+                var button = (Button)sender;
+                button.Content = new MaterialDesignThemes.Wpf.PackIcon { Kind = MaterialDesignThemes.Wpf.PackIconKind.Eye };
+                IsPasswordShow = false;
+            }
+        }
+        private bool IsPasswordShow = false;
+
+    }
+}
